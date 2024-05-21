995d6e52fc94f2a83797a0a251c23799cb5a668e
Renz Bibe <93276204+renzbibe@users.noreply.github.com>
2024-05-21 11:09:36 +0100

    Update products.js

--- scripts/products.js
+++ scripts/products.js
@@ -7,7 +7,7 @@ export function displayProducts(productList, htmlDisplay, buttonName, productNam
     <div class="col-6 col-md-4 col-lg-3 mb-4">
           <div class="card border-1">
             <div class="card-body text-center product-container">
-              <h1 class="card-title h4 product-name d-flex justify-content-center align-items-center">${[product.name]}</h1>
+              <h1 class="card-title h4 product-name d-flex justify-content-center align-items-center">${product.brand} ${product.name}</h1>
               <img src="${product.image}" alt="" class="img-fluid product-image">
               <p class="card-text text-muted product-description">${product.description}</p>
               <div class="d-flex justify-content-between align-items-center px-1 px-lg-4">
